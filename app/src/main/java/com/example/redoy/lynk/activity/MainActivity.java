33e4d3bf3334ec7b05c78efd9674989d94a0e38c
redoy <redoyahmed@gmail.com>
2018-03-30 15:37:19 +0600

    second commit

--- app/src/main/java/com/example/redoy/lynk/activity/MainActivity.java
+++ app/src/main/java/com/example/redoy/lynk/activity/MainActivity.java
@@ -14,51 +14,47 @@ import com.example.redoy.lynk.fragment.GoogleMapSearchFragment;
 import com.example.redoy.lynk.fragment.VoiceSearchFragment;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.bottom_navigation)
     BottomNavigationView bottomNavigationView;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-            Fragment mFragment = null;
-
-            switch (item.getItemId()) {
-                case R.id.navigation_voice_search: {
-                    mFragment = new VoiceSearchFragment();
-                    getSupportActionBar().setTitle(R.string.title_fragment_voice_search);
-                    break;
-                }
-                case R.id.navigation_map_search: {
-                    mFragment = new GoogleMapSearchFragment();
-                    getSupportActionBar().setTitle(R.string.title_fragment_google_map_search);
-                    break;
-                }
-                case R.id.navigation_settings: {
-                    mFragment = new FragmentSettings();
-                    getSupportActionBar().setTitle(R.string.title_fragment_settings);
-                    break;
-                }
-            }
-            FragmentManager mFragmentManager = getSupportFragmentManager();
-            if (mFragment != null) {
-                mFragmentManager.beginTransaction().replace(R.id.frame_layout, mFragment).commit();
-            }
-            return true;
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment mFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.navigation_voice_search: {
+                        mFragment = new VoiceSearchFragment();
+                        getSupportActionBar().setTitle(R.string.title_fragment_voice_search);
+                        break;
+                    }
+                    case R.id.navigation_map_search: {
+                        mFragment = new GoogleMapSearchFragment();
+                        getSupportActionBar().setTitle(R.string.title_fragment_google_map_search);
+                        break;
+                    }
+                    case R.id.navigation_settings: {
+                        mFragment = new FragmentSettings();
+                        getSupportActionBar().setTitle(R.string.title_fragment_settings);
+                        break;
+                    }
+                }
+                FragmentManager mFragmentManager = getSupportFragmentManager();
+                if (mFragment != null) {
+                    mFragmentManager.beginTransaction().replace(R.id.frame_layout, mFragment).commit();
+                }
+                return true;
+            }
+        });
     }
-
 }
